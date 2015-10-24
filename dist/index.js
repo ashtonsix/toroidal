f7503dd092f8bef0d1dfa85868251c928e46eb3c
Ashton War <me@ashtonwar.com>
2015-10-24 12:18:33 +0100

    Update to latest build

--- dist/index.js
+++ dist/index.js
@@ -26,6 +26,78 @@ var splice = function splice(arr, replaceFrom, newArr) {
   return start < end ? concat(arr.slice(0, start), newArr, arr.slice(end)) : concat(newArr.slice(arr.length - start), arr.slice(end, start), newArr.slice(0, arr.length - start));
 };
 
+var unwrap = function unwrap(v) {
+  return v._torrodial ? v.value() : v;
+};
+
+var chainable = function chainable(wrapper, data, f) {
+  return function chained() {
+    return wrapper(f.bind(null, data).apply(null, arguments));
+  };
+};
+
+var subset = function subset(d, x, y, width, height) {
+  var data = unwrap(d);
+
+  return slice(data, y, y + height).map(function (row) {
+    return slice(row, x, x + width);
+  });
+};
+
+exports.subset = subset;
+var insert = function insert(d, x, y, nd) {
+  var newData = unwrap(nd);
+  var data = unwrap(d);
+
+  if (newData.length > data.length) {
+    console.error('Cannot insert data: will not fit');
+    return null;
+  }
+
+  return splice(data, y, slice(data, y, y + newData.length).map(function (row, yi) {
+    return splice(row, x, newData[yi]);
+  }));
+};
+
+exports.insert = insert;
+var map = function map(d, f) {
+  var data = unwrap(d);
+  return data.map(function (row, y) {
+    return row.map(function (value, x) {
+      return f(value, x, y);
+    });
+  });
+};
+
+exports.map = map;
+var reduce = function reduce(d, f, initialValue) {
+  var data = unwrap(d);
+  function step(_x, _x2, _x3) {
+    var _again = true;
+
+    _function: while (_again) {
+      var pv = _x,
+          x = _x2,
+          y = _x3;
+      newX = newY = undefined;
+      _again = false;
+
+      if (y >= data.length) return pv;
+      var newX = (x + 1) % data[0].length;
+      var newY = newX < x ? y + 1 : y;
+      _x = f(pv, data[y][x], x, y, data);
+      _x2 = newX;
+      _x3 = newY;
+      _again = true;
+      continue _function;
+    }
+  }
+
+  return step(initialValue, 0, 0);
+};
+
+exports.reduce = reduce;
+
 function torodial(data) {
   return {
     _torrodial: true,
@@ -34,57 +106,8 @@ function torodial(data) {
       return data;
     },
 
-    subset: function subset(x, y, width, height) {
-      return torodial(slice(data, y, y + height).map(function (row) {
-        return slice(row, x, x + width);
-      }));
-    },
-
-    insert: function insert(x, y, nd) {
-      var newData = nd._torrodial ? nd.value() : nd;
-
-      if (newData.length > data.length) {
-        console.error('Cannot insert data: will not fit');
-        return null;
-      }
-
-      return torodial(splice(data, y, slice(data, y, y + newData.length).map(function (row, yi) {
-        return splice(row, x, newData[yi]);
-      })));
-    },
-
-    map: function map(f) {
-      return torodial(data.map(function (row, y) {
-        return row.map(function (value, x) {
-          return f(value, x, y);
-        });
-      }));
-    },
-
-    reduce: function reduce(f, initialValue) {
-      function reduce(_x, _x2, _x3) {
-        var _again = true;
-
-        _function: while (_again) {
-          var pv = _x,
-              x = _x2,
-              y = _x3;
-          newX = newY = undefined;
-          _again = false;
-
-          if (y >= data.length) return pv;
-          var newX = (x + 1) % data[0].length;
-          var newY = newX < x ? y + 1 : y;
-          _x = f(pv, data[y][x], x, y, data);
-          _x2 = newX;
-          _x3 = newY;
-          _again = true;
-          continue _function;
-        }
-      }
-
-      return reduce(initialValue, 0, 0);
-    } };
+    subset: chainable(torodial, data, subset),
+    insert: chainable(torodial, data, insert),
+    reduce: reduce.bind(null, data),
+    map: chainable(torodial, data, map) };
 }
-
-module.exports = exports['default'];
